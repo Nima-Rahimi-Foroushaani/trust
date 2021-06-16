6cae04e9d38a5488fc740429dff2fc6a4d222171
Nima-Rahimi-Foroushaani <nimacodecont@gmail.com>
2021-06-16 16:39:41 +0200

    Progress execution works

--- trust.ml
+++ trust.ml
@@ -3,30 +3,48 @@ type mem_loc = string
 
 type mutability =
    MtImm
-   |MtMut
+   | MtMut
 
-   (* @todo add a data structure which prevents duplicate keys but similar to assoc list preserves the order *)
 type typ =
    TypProd of id * (id * typ) list
-   |TypLoc
+   | TypLoc
 let typ_unit = TypProd("Unit", [])
 
 type vlu =
    VluProd of id * (id * vlu) list
-   |VluLoc of mem_loc
+   | VluLoc of mem_loc
 let vlu_unit = VluProd("unit", [])
 
 type term =
    TmVal of vlu
-   |TmVar of id
-   |TmDref of term
-   |TmLet of mutability * id * typ * term
-   |TmSeq of term * term
+   | TmVar of id
+   | TmDref of term
+   | TmLet of mutability * id * typ * term
+   | TmSeq of term * term
 
-type mem_grd = {rd:int; wr:int}
+module type Perm_type = sig
+   type perm = PermDel | PermWr | PermRd | PermDen
+   val (<) : perm -> perm -> bool
+   val (>=) : perm -> perm -> bool
+end
+
+module Perm: Perm_type = struct
+   type perm = PermDel | PermWr | PermRd | PermDen
+
+   let weight = function
+   | PermDen ->   0
+   | PermRd ->    1
+   | PermWr ->    2
+   | PermDel ->   3
+
+   let (<) a b = weight a < weight b
+   let (>=) a b = weight a >= weight b
+end
+
+open Perm
 
 type sym_store = (id * mem_loc) list
-type sym_mem_stage = (mem_loc * vlu) list
+type sym_mem_stage = (mem_loc * (vlu * perm)) list
 type sym_exe_ctx = sym_store * sym_mem_stage
 
 type stuck =
@@ -37,39 +55,42 @@ type stuck =
 
 type exec_rsl =
    ExRslProgress
-   |ExRslVlu
-   |ExRslStuk of stuck
+   | ExRslVlu
+   | ExRslStuk of stuck
 
-let rec sym_exec_step t ((sstore, smstage) as sctx:sym_exe_ctx) =
+let rec sym_exe_step t ((sstore, smstage) as sctx:sym_exe_ctx) =
 match t with
 | TmVal(_) -> ExRslVlu, t, sctx
-| TmVar(n) -> (
-   match List.assoc_opt n sstore with
-   | Some(l) -> ExRslProgress, TmDref(TmVal(VluLoc(l))), sctx
-   | None -> ExRslStuk(StkUnboundVar), t, sctx
-   )
-| TmDref(t1) -> (
-   match (t1) with
-   | TmVal(VluLoc(l)) -> (
-      match List.assoc_opt l smstage with
-      | Some(v) -> ExRslProgress, TmVal(v), sctx
-      | None -> ExRslStuk(StkNpermAcs), t, sctx
-      )
-   | TmVal(_) -> ExRslStuk(StkUndefBeh), t, sctx
-   | _ -> let (rsl, t1', sctx') = sym_exec_step (t1) sctx in (rsl, TmDref(t1'), sctx')
-   )
-| TmLet(mty, n, ty, t1) -> (
-   match t1 with
-   (*todo: fresh symbol, check name for duplicate*)
-   | TmVal(v) -> ExRslProgress, TmVal(vlu_unit), let l = "l" in ((n, l)::sstore, (l, v)::smstage)
-   | _ -> let (rsl, t1', sctx') = sym_exec_step t1 sctx in (rsl, TmLet(mty, n, ty, t1'), sctx')
-)
-| TmSeq(t1, t2) -> (
-   match t1 with
-   | TmVal(_) -> ExRslProgress, t2, sctx
-   | _ -> let (rsl, t1', sctx') = sym_exec_step t1 sctx in (rsl, TmSeq(t1', t2), sctx')
-)
-(*| _ -> ExRslStuk(StkNotSupp), t,sctx*)
+| TmVar(n) -> (match List.assoc_opt n sstore with
+               | Some(l) -> ExRslProgress, TmDref(TmVal(VluLoc(l))), sctx
+               | None -> ExRslStuk(StkUnboundVar), t, sctx
+               )
+| TmDref(t1) -> (match t1 with
+                  | TmVal(v) -> (match v with
+                                 | VluLoc(l) -> (match List.assoc_opt l smstage with
+                                                | Some(v, p) when p >= PermRd -> ExRslProgress, TmVal(v), sctx
+                                                | _ -> ExRslStuk(StkNpermAcs), t, sctx
+                                                )
+                                 | _ -> ExRslStuk(StkUndefBeh), t, sctx
+                                 )
+                  | _ -> let (rsl, t1',sctx') = sym_exe_step t1 sctx in rsl, TmDref(t1'), sctx'
+                  )
+| TmLet(mty, n, ty, t1) -> (match t1 with
+                           (*todo: provide fresh symbol, check name for duplicate*)
+                           | TmVal(v) -> ExRslProgress, TmVal(vlu_unit), let l = "l" in ((n, l)::sstore, (l, (v, PermDel))::smstage)
+                           | _ -> let (rsl, t1', sctx') = sym_exe_step t1 sctx in (rsl, TmLet(mty, n, ty, t1'), sctx')
+                           )
+| TmSeq(t1, t2) -> (match t1 with
+                     | TmVal(_) -> ExRslProgress, t2, sctx
+                     | _ -> let (rsl, t1', sctx') = sym_exe_step t1 sctx in (rsl, TmSeq(t1', t2), sctx')
+                     )
+
+let rec sym_exe t sctx = 
+   let (rsl_s, t', sctx') as res = sym_exe_step t sctx in
+   match rsl_s with
+   | ExRslProgress -> sym_exe t' sctx'
+   | ExRslVlu
+   | ExRslStuk(_) -> res
 
 (*let main() = 
 Printf.printf "tRust 1.0.0\n";
@@ -82,7 +103,7 @@ let simple_imm_ref_read =
    )
 ;;
 
-sym_exec_step simple_imm_ref_read ([], [])
+sym_exe simple_imm_ref_read ([], [])
 
 (*exit 0;;
 main();;*)
